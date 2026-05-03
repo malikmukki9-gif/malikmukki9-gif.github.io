0f795a491a67357a417b291fa3991c287192d383
malikmukki9-gif <malikmukki9@gmail.com>
2026-05-03 12:46:31 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Hier starten wir die Animations-Bibliothek
+AOS.init({
+    // Dauer der Animation in Millisekunden (1000ms = 1 Sekunde)
+    duration: 1000, 
+    
+    // Animation wird nur einmal abgespielt, nicht bei jedem hoch/runter scrollen
+    once: true, 
+    
+    // Startet die Animation etwas früher/später im Bildschirm
+    offset: 100 
+});
